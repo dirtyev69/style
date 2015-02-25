708d5fdc7d96dfbc6565f745617daaaf7acea7df
Evgeny Antipin <jeka.antipin@jetrockets.ru>
2015-02-25 21:23:26 +0300

    add seo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def seo_title(value)
+    content_for(:seo_title, value + ' / ' + default_seo_title)
+  end
+
+  def seo_description(value)
+    content_for(:seo_description, value)
+  end
+
+  def seo_keywords(value)
+    content_for(:seo_keywords, value)
+  end
+
+  def default_seo_title
+    "Обувь Стиль — обувной магазин в городе Твери"
+  end
 end
